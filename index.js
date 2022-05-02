9169fa34251dce22b0724edf29a01769a48174e8
Micaela Frank <micaelalfrank@gmail.com>
2022-05-02 12:49:43 -0400

    commit tree eea9cb5442858a996f6b49e60abaf462aa1583c0

---
+++ index.js
@@ -0,0 +1,62 @@
+
+/*const square = x => x**2;
+console.log(square(4));
+
+const add = (num1, num2) => num1 + num2;
+console.log(add(6, 7));
+
+// const divide = (num1, num2) => num1/num2;
+// console.log(divide(2000, 100));
+
+
+
+function divide(){
+    const dividedNums = 2000/100;
+    console.log(dividedNums);
+    return dividedNums;
+}
+
+*/
+
+
+
+
+// const divide = () => 2000 / 100;
+
+// const square = (x) => x*x;
+
+// const add = (num1, num2) => num1 + num2;
+
+
+
+
+
+// function divide(){
+//     return 2000/100;
+// }
+
+// const divide = function(){
+//     return 2000/100;
+// }
+
+const divide = () => 2000/100;
+console.log(divide());
+
+// function square(x){
+//     return x**2;
+// }
+
+// const square = function(x){
+//     return x*2;
+// }
+
+const square = x => x**2;
+console.log(square(6));
+
+// function add(x, y){
+//     console.log(x+y);
+//     return x+y;
+// }
+
+const add = (x, y) => x+y;
+console.log(add(7, 21));
